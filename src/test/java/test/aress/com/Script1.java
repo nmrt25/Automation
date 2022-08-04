95c41c42fe6f4d136fb03d79dbf65220f46f7574
mayur-more-aress <mayur.more@aressindia.net>
2022-08-04 15:09:05 +0530

    added remote webdriver script

--- src/test/java/test/aress/com/Script1.java
+++ src/test/java/test/aress/com/Script1.java
@@ -1,12 +1,24 @@
 package test.aress.com;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 public class Script1 {
 	
 @Test
-public void method1(){
+public void method1() throws MalformedURLException{
 		
-		System.out.println("Script1 is Run successfully................");
+	ChromeOptions chromeOptions = new ChromeOptions();
+
+	WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/"), chromeOptions);
+	driver.get("http://www.google.com");
+	driver.quit();
+		
+		
 	}
 }
